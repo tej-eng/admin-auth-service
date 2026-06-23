5f063463c0141ea31985e1522c9f674362f1bac9
AmmyAstro <amrender@dhwaniastro.com>
2026-06-23 13:11:31 +0530

    dobb

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -751,7 +751,7 @@ const typeDefs = gql`
     skills: [String]
     languages: [String]
     problems: [String]
-    dob: [DateTime]
+    dob: DateTime
     experience: Int
     applicationStatus: String!
     interviewStatus: String
