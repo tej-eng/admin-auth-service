7689293a77631b8bc99d1adac6024d826ac8ffa1
AmmyAstro <amrender@dhwaniastro.com>
2026-06-23 12:50:18 +0530

    dob

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -751,7 +751,7 @@ const typeDefs = gql`
     skills: [String]
     languages: [String]
     problems: [String]
-
+dateOfBirth
     experience: Int
     applicationStatus: String!
     interviewStatus: String
